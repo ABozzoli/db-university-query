baee35083b41c4002f60519d5f32c9f7c7dbfb9e
ABozzoli <alessandrobozzoli92@gmail.com>
2021-12-15 15:37:39 +0100

    Punti 2 e 3

--- university_query.sql
+++ university_query.sql
@@ -2,7 +2,11 @@
 SELECT * FROM students WHERE YEAR(date_of_birth) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT * FROM courses WHERE cfu > 10;
+
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM students WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) > 30; 
+
 -- 4. Selezionare tutti i corsi di laurea magistrale
 -- 5. Da quanti dipartimenti è composta l’università?
 -- 6. Quanti sono gli insegnanti che non hanno un numero di telefono?
